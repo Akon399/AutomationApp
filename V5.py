f9cffb17b23330d8de4d97a409b85c662064d916
Akon399 <73472319+Akon399@users.noreply.github.com>
2023-12-05 19:10:52 +0200

    Add files via upload

--- V5.py
+++ V5.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Set up the page
 st.set_page_config(page_title='Risk App')
-st.title('Automation Application.')
+st.title('Risk Automation Application.')
 st.subheader('Upload Merchant Data:')
 
 # File uploader
@@ -44,25 +44,38 @@ if st.button("PV1") and required_columns_exist(['Card Brand', 'Status'], df):
     pivot_table_pv1['Acceptance Rate'] = pivot_table_pv1['approved'] / (pivot_table_pv1['Grand Total'] - pivot_table_pv1['error'] - pivot_table_pv1['refunded'])
     st.write(pivot_table_pv1 * 100)
     csv_data = pivot_table_pv1.to_csv(index=True)
-    st.download_button(label="Download PV1 as CSV", data=csv_data, file_name="pivot_table_pv1.csv")
+    st.download_button(label="Download PV1", data=csv_data, file_name="pivot_table_pv1.csv")
 
 # Pivot Table 2
 if st.button("PV2") and required_columns_exist(['Type', 'Merchant', 'ErrorMessage', 'Merchant Transaction id'], df):
-    filtered_df = df[df['Type'] == 'sale3d']
-    pivot_table_pv2 = pd.pivot_table(filtered_df, index=['Merchant', 'ErrorMessage'], values=['Merchant Transaction id'], aggfunc={'Merchant Transaction id': 'count'})
-    pivot_table_pv2.columns = ['Count of Merchant Transaction id']
-    pivot_table_pv2['Percentage Rate'] = (pivot_table_pv2['Count of Merchant Transaction id'] / pivot_table_pv2['Count of Merchant Transaction id'].sum()) * 100
-    st.write(pivot_table_pv2)
-    csv_data = pivot_table_pv2.to_csv(index=True)
-    st.download_button(label="Download PV2 as CSV", data=csv_data, file_name="pivot_table_pv2.csv")
+    # Extract the last 4 digits from the 'Merchant' column
+    df['Merchant Group'] = df['Merchant'].str.extract(r'(.+? - \d{4})$')
+
+    # Group by the merchant group and error message, then count occurrences
+    pivot_table_pv2 = df[df['Type'] == 'sale3d'].groupby(['Merchant Group', 'ErrorMessage']).size().reset_index(name='Count of Merchant Transaction id')
+
+    # Calculate Percentage Rate
+    total_counts = pivot_table_pv2.groupby('Merchant Group')['Count of Merchant Transaction id'].sum().reset_index()
+    pivot_table_pv2 = pivot_table_pv2.merge(total_counts, on='Merchant Group', suffixes=('', '_total'))
+    pivot_table_pv2['Percentage Rate'] = (pivot_table_pv2['Count of Merchant Transaction id'] / pivot_table_pv2['Count of Merchant Transaction id_total']) * 100
+
+    # Remove duplicates in 'Merchant Group' to make it unique
+    pivot_table_pv2 = pivot_table_pv2.drop_duplicates(subset='Merchant Group', keep='first')
+
+    st.write(pivot_table_pv2.drop('Count of Merchant Transaction id_total', axis=1))
+    csv_data = pivot_table_pv2.drop('Count of Merchant Transaction id_total', axis=1).to_csv(index=False)
+    st.download_button(label="Download PV2", data=csv_data, file_name="pivot_table_pv2.csv")
+
 
 # Pivot Table 3
 if st.button("PV3") and required_columns_exist(['BIN country', 'Status', 'Transaction id'], df):
     pivot_table_pv3 = df.pivot_table(index='BIN country', columns='Status', values='Transaction id', aggfunc='count', fill_value=0)
     st.write(pivot_table_pv3)
     csv_data = pivot_table_pv3.to_csv(index=True)
-    st.download_button(label="Download PV3 as CSV", data=csv_data, file_name="pivot_table_pv3.csv")
+    st.download_button(label="Download PV3", data=csv_data, file_name="pivot_table_pv3.csv")
 
 # Footer with a link
 st.markdown("---")
-st.markdown('Powered by [EMpay](https://www.emerchantpay.com/)')
+st.markdown('Powered by [emerchantpay.com/](https://www.emerchantpay.com/)')
+
+
